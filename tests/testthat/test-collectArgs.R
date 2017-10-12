e42ca37064329048c2e5f4c51bf021203009a36e
Rick Saporta <rick.saporta@vydia.com>
2017-10-12 11:43:28 -0400

    More tests added

--- tests/testthat/test-collectArgs.R
+++ tests/testthat/test-collectArgs.R
@@ -1,47 +1,47 @@
-context("args_collected")
+context("Checking collectArgs")
 
 test_that("Args collected correctly", {
-  test_func <- function(x, second_arg=1:5, third_arg=NULL, fourth_arg=c("hello world")) {
-    cat("test_func_executed\n")
-
-    dont_use_me_when_iterating <- "I should not have been used"
-    # i_will_cause_failure <- "jkhjkhkjh"
-
-    if (is.list(x)) {
-      return(iterateWithArgs(x, FUNC=test_func, except="dont_use_me_when_iterating"))
-    }
-
-  return(x^2)
+  main_function <- function(x, param_two, param_three, param_four) {
+    sprintf("x has length %i. param_two has length %i. param_three has length %i. param_four has length %i.", 
+        length(x), length(param_two), length(param_three), length(param_four)
+    )
   }
 
-  ## vectors
-  expect_equal(test_func(1:4), c(1, 4, 9, 16))
-  expect_output(test_func(1:4), "test_func_executed")
-
-  ## list
-  expect_equal(test_func(as.list(1:4)), list(1, 4, 9, 16))
-  expect_output(test_func(as.list(1:4)), c("test_func_executed\ntest_func_executed\ntest_func_executed\ntest_func_executed\ntest_func_executed"))
-})
-
-test_that("Error Thrown Correctly", {
-  test_func_will_error_only_on_list <- function(x, second_arg=1:5, third_arg=NULL, fourth_arg=c("hello world")) {
-    cat("test_func_executed\n")
-
-    dont_use_me_when_iterating <- "I should not have been used"
-    i_will_cause_failure <- "jkhjkhkjh"
-
-    if (is.list(x)) {
-      return(iterateWithArgs(x, FUNC=test_func, except="dont_use_me_when_iterating"))
-    }
-
-  return(x^2)
+  calling_func_1 <- function(x, param_two, param_three, param_four) {
+    args <- collectArgs()
+    do.call(main_function, args)  
   }
 
-  ## vectors
-  expect_equal(test_func_will_error_only_on_list(1:4), c(1, 4, 9, 16))
-  expect_output(test_func_will_error_only_on_list(1:4), "test_func_executed")
+  calling_func_2_erorred <- function(x, param_two, param_three, param_four, param_five) {
+    args <- collectArgs()
+    do.call(main_function, args)
+  }
 
-  ## list
-  expect_error(test_func_will_error_only_on_list(as.list(1:4)))
+  calling_func_2 <- function(x, param_two, param_three, param_four, param_five) {
+    args <- collectArgs(except="param_five")
+    do.call(main_function, args)
+  }
+
+  calling_func_3_errored <- function(x, param_two, param_four) {
+    args <- collectArgs()
+    do.call(main_function, args)
+  }
+
+  calling_func_3 <- function(x, param_two, param_four) {
+    param_three <- "param described in function"
+    args <- collectArgs()
+    do.call(main_function, args)
+  }
+
+  output_expected <- "x has length 5. param_two has length 3. param_three has length 1. param_four has length 0."
+  expect_equal(object=calling_func_1(1:5, c("a", "b", "c"), base::paste, as.integer()),  output_expected)
+
+  expect_error(calling_func_2_erorred(1:5, c("a", "b", "c"), base::paste, as.integer(), param_five="some garbage"))
+  expect_equal(calling_func_2(1:5, c("a", "b", "c"), base::paste, as.integer(), param_five="some garbage"), output_expected)
+
+  expect_error(calling_func_3_erorred(1:5, c("a", "b", "c"), base::paste))
+  expect_equal(calling_func_3(1:5, c("a", "b", "c"), as.integer()), output_expected)
+
 })
 
+## TODO:  More tests wiht parameters for collectArgs
